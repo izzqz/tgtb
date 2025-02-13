05cd64d447bcc36d03e3e2001e9bfcaae698e7b6
izzqz <v@izzqz.me>
2025-02-13 03:48:44 +0300

    Add working generic

--- src/factories/call_method.ts
+++ src/factories/call_method.ts
@@ -1,12 +1,19 @@
 import { type TgtbOptions } from "../mod.ts";
 import type { ApiMethods } from "../types/telegram.ts";
 
+type BotMethodKeys = keyof ApiMethods<unknown>;
+type BotMethodParams<M extends BotMethodKeys> = Parameters<ApiMethods<unknown>[M]>[0];
+type BotMethodReturn<M extends BotMethodKeys> = ReturnType<ApiMethods<unknown>[M]>;
+
 export default function buildCallMethod(
   bot_token: string,
   options: Required<TgtbOptions>,
 ) {
   const { fetch_fn, base_url } = options;
-  return async (method: BotMethod, params?: any) => {
+  return async <M extends BotMethodKeys>(
+    method: M,
+    params?: BotMethodParams<M>,
+  ): Promise<BotMethodReturn<M>> => {
     const url = new URL(base_url.concat(bot_token, "/", method));
 
     // append params
